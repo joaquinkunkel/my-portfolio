aa6c9d55f89ac70795fcffd746c20d9a041452c2
Joaquin Kunkel <jek537@nyu.edu>
2024-08-28 19:08:38 -0600

    Redirect www

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.joaquin.world', // Replace with your actual domain
+          },
+        ],
+        destination: 'https://joaquin.world/*',
+        permanent: true,
+      },
+    ];
+  },
+  webpack(config) {
+    // Find the rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
 
+    // Exclude SVGs from the file loader
+    fileLoaderRule.exclude = /\.svg$/;
+
+    // Add a new rule to handle SVGs with @svgr/webpack
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
-export default {
-    webpack(config) {
-      // Find the rule that handles SVG imports
-      const fileLoaderRule = config.module.rules.find((rule) =>
-        rule.test?.test?.('.svg')
-      );
-  
-      // Exclude SVGs from the file loader
-      fileLoaderRule.exclude = /\.svg$/;
-  
-      // Add a new rule to handle SVGs with @svgr/webpack
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-  
-      return config;
-    },
-  };
-  
+export default nextConfig;
