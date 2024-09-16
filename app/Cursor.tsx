9e103339585ad8b697d85a0093feaa6424c386dd
Joaquin Kunkel <jek537@nyu.edu>
2024-09-16 08:54:41 -0600

    Fix build

--- app/Cursor.tsx
+++ app/Cursor.tsx
@@ -5,7 +5,7 @@ const Cursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (e: any) => {
       setPosition({ x: e.clientX, y: e.clientY });
     };
 
